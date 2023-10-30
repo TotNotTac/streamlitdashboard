a1e25ab446a4df517ae8dd1c9247ce3a2b86ff3c
Silas de Jong <andorthekid@gmail.com>
2023-10-30 12:05:19 +0100

    increase upload size, add datasets, and add caching

--- amsterdamverkeer/main.py
+++ amsterdamverkeer/main.py
@@ -1,6 +1,7 @@
 
 import streamlit as st
 import folium
+from folium.plugins import MarkerCluster
 from streamlit_folium import st_folium
 
 import plotly.express as px
@@ -9,6 +10,35 @@ import pandas as pd
 import io
 
 import seaborn as sns
+
+
+@st.cache_data
+def load_transactions():
+    transacties = pd.read_csv('laadpaaldata.csv')
+
+    transacties = transacties.query('`ChargeTime` >= 0')
+
+    transacties['Ended'] = pd.to_datetime(
+        transacties['Ended'], errors='coerce')
+    transacties['Started'] = pd.to_datetime(
+        transacties['Started'], errors='coerce')
+    return transacties
+
+
+@st.cache_data
+def load_laadpalen():
+    return pd.read_json('./laadpalen.json')
+
+
+@st.cache_data
+def load_laadpalen_small():
+    return pd.read_json('./laadpalen_small.json')
+
+
+@st.cache_data
+def load_elektrische_voertuigen():
+    return pd.read_csv('./Elektrische_voertuigen_20231030.csv')
+
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
@@ -20,16 +50,10 @@ data_load_state = st.text('Loading data...')
 
 
 # Load data from csv file
-laadpalen = pd.read_json('./laadpalen.json')
-laadpalen_small = pd.read_json('./laadpalen_small.json')
-transacties = pd.read_csv('laadpaaldata.csv')
-
-transacties = transacties.query('`ChargeTime` >= 0')
-
-transacties['Ended'] = pd.to_datetime(
-    transacties['Ended'], errors='coerce')
-transacties['Started'] = pd.to_datetime(
-    transacties['Started'], errors='coerce')
+laadpalen = load_laadpalen()
+laadpalen_small = load_laadpalen_small()
+transacties = load_transactions()
+voertuigen = load_elektrische_voertuigen()
 
 data_load_state.text('')
 
@@ -37,7 +61,24 @@ data_load_state.text('')
 ### Raw data
 """
 
-transacties
+voertuigen
+
+# transacties
+
+sns.histplot(transacties, x="TotalEnergy")
+st.pyplot()
+
+sns.boxplot(transacties, x="TotalEnergy")
+st.pyplot()
+
+sns.boxplot(transacties, x="ConnectedTime")
+st.pyplot()
+
+sns.boxplot(transacties, x="ChargeTime")
+st.pyplot()
+
+sns.boxplot(transacties, x="MaxPower")
+st.pyplot()
 
 logscale = st.checkbox("Logarithmic scale")
 overlay = st.checkbox("Overlay plots")
@@ -71,7 +112,7 @@ st.pyplot()
 
 AMSTERDAM_LAT_LONG = (52.377956, 4.897070)
 
-smallSet = st.checkbox("Use small dataset", value=True)
+smallSet = st.checkbox("Use small dataset", value=False)
 
 # Create and show empty Folium map
 m = folium.Map(location=AMSTERDAM_LAT_LONG, zoom_start=12,
@@ -79,15 +120,17 @@ m = folium.Map(location=AMSTERDAM_LAT_LONG, zoom_start=12,
 
 targetSet = laadpalen_small if smallSet else laadpalen
 
+
+cluster = MarkerCluster().add_to(m)
 for index, row in targetSet.iterrows():
     address = row['AddressInfo']
 
     color = 'green' if row['IsRecentlyVerified'] else 'blue'
 
-    folium.Circle(location=[address['Latitude'], address['Longitude']],
+    folium.Marker(location=[address['Latitude'], address['Longitude']],
                   tooltip=address['AddressLine1'],
                   color=color
-                  ).add_to(m)
+                  ).add_to(cluster)
 
 """
 Charging stations throughout the Netherlands
