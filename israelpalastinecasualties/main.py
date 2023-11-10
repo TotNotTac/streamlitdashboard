d4194515190695531f7da2ab0ced53e52ad529ca
Jan <janlangenhuijzen@icloud.com>
2023-11-10 10:04:52 +0100

    jan(2)toegevoegd

--- israelpalastinecasualties/main.py
+++ israelpalastinecasualties/main.py
@@ -1,15 +1,21 @@
 
 import streamlit as st
 from exploration import *
+from jan import jan
+from jan2 import jan2
 
 st.set_page_config(layout="wide")
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 selectedPlot = st.sidebar.selectbox(
     "Select plot",
-    ("Data exploration", ""),
+    ("Data exploration", "jan", "jan2"),
 )
 
 match selectedPlot:
     case "Data exploration":
         exploration_plot()
+    case "jan":
+        jan()
+    case "jan2":
+        jan2()
